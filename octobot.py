7c0e6747e2ff3b438e1cd53c59a767f901d98cc4
Soumitra Shewale <soumitra.shewale.dev@gmail.com>
2020-10-05 12:20:43 +0530

    :sparkles: Add repo info endpoint

--- octobot.py
+++ octobot.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import os
 
@@ -14,6 +15,16 @@ gh_api_header = {"Accept": "application/vnd.github.v3+json"}
 octobot = commands.Bot(command_prefix=octobot_prefix, description=description)
 
 
+# Helper funcs
+
+async def format_time(in_str_time):
+    return datetime.datetime.strptime(
+        in_str_time, "%Y-%m-%dT%H:%M:%SZ"
+    ).strftime("%mᵗʰ %b '%y %H:%M")
+
+
+# Discord funcs
+
 @octobot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(octobot))
@@ -24,6 +35,101 @@ async def on_message(message):
     if message.author == octobot.user:
         return
     await octobot.process_commands(message)
+
+
+@octobot.command()
+async def repo(ctx, owner_user, repo_name):
+    url = "https://api.github.com/repos/{0}/{1}".format(owner_user, repo_name)
+    res = requests.get(url, headers=gh_api_header)
+    if res.status_code == 200:
+        data = res.json()
+        res_embed = discord.Embed(
+            title=data['name'],
+            url=data['html_url']
+        )
+
+        if data['description']:
+            res_embed.add_field(name="Description",
+                                value=data['description'], inline=False)
+        res_embed.set_thumbnail(url=data['owner']['avatar_url'])
+
+        res_embed.add_field(name="Language", value="{0}".format(
+            data['language']
+        ))
+
+        res_embed.add_field(name="License", value=("[{0}]({1})".format(
+            data['license']['name'],
+            data['html_url'] + '/blob/' + data['default_branch'] + '/LICENSE')
+            if data['license'] else "None")
+        )
+
+        res_embed.add_field(name="Open Issues",
+                            value="[{0}]({1})".format(
+                                data["open_issues_count"],
+                                data['html_url'] + "/issues")
+                            )
+
+        res_embed.add_field(name="Stars",
+                            value="[{0}]({1})".format(
+                                data["stargazers_count"],
+                                data['html_url'] + "/stargazers")
+                            )
+
+        res_embed.add_field(name="Watchers",
+                            value="[{0}]({1})".format(
+                                data["watchers_count"],
+                                data['html_url'] + "/watchers")
+                            )
+
+        res_embed.add_field(name="Forks",
+                            value="[{0}]({1})".format(
+                                data["network_count"],
+                                data['html_url'] + "/network/members")
+                            )
+
+        res_embed.add_field(name="Default Branch",
+                            value="[{0}]({1})".format(
+                                data["default_branch"],
+                                data['html_url'] + "/tree/" +
+                                data['default_branch'])
+                            )
+
+        contributors = ["[{0}]({1})".format(x['login'], "https://github.com/" +
+                                            x['login']) for x in requests.get(
+            data['contributors_url'], headers=gh_api_header).json()]
+
+        if contributors:
+            res_embed.add_field(
+                name="Contributor" + ("s" if len(contributors) > 1 else ""),
+                value=" and ".join(contributors) if len(contributors) <= 2 else
+                (", ".join(contributors[:2]) + ", and {0} others".format(
+                    len(contributors) - 2)
+                 )
+            )
+
+        res_embed.add_field(name="Clone URL", value="`{0}`".format(
+            data["clone_url"]), inline=False)
+
+        res_embed.add_field(name="SSH URL", value="`{0}`".format(
+            data["ssh_url"]), inline=False)
+
+        res_embed.set_footer(
+            text="Created: {0} \nLast Commit: {1} \nLast Push: {2}".format(
+                await format_time(data['created_at']),
+                await format_time(data['updated_at']),
+                await format_time(data['pushed_at'])
+            )
+        )
+
+    elif res.status_code == 404:
+        res_embed = discord.Embed(
+            title="Repo not found",
+            description="Repo `{0}` owned by `{1}` was not found.".format(
+                repo_name, owner_user),
+            color=0xFF0000
+        )
+
+    await ctx.send(embed=res_embed)
 
 
 @octobot.command()
@@ -97,6 +203,13 @@ async def user(ctx, username):
             res_embed.add_field(
                 name="Twitter", value="[{0}](https://twitter.com/{0})".format(
                     data["twitter_username"]))
+
+        res_embed.set_footer(
+            text="Created: {0} \nLast Commit: {1}".format(
+                await format_time(data['created_at']),
+                await format_time(data['updated_at'])
+            )
+        )
 
     elif res.status_code == 404:
         res_embed = discord.Embed(
